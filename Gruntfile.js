79bacd037a1ba02e91a70c978037b1857407215e
organnoise <dm09ke@student.ocadu.ca>
2014-10-06 12:40:45 -0400

    looks pretty good

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,7 +291,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'fonts/{,*/}*.*'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',

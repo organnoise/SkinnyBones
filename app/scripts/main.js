8bd416d793a840b58dbeac318005c60307d8c089
organnoise <dm09ke@student.ocadu.ca>
2014-10-07 01:33:04 -0400

    Title 15px on resize

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,22 @@
+function clipName() {
+	var appDistance = $('#friend').offset().top - 40;
+	
+    $('#parallax').scroll(function() {
+        // scrollTop gets the current vertical position of the scroll bar for the first element in the set of matched elements or set the vertical position of the scroll bar for every matched element.
+        if ($('#parallax').scrollTop() >= appDistance) {
+            // If the window scrollbar position is greater or equal to the position of the 'app' element then add class navbar-ash to the navbar
+            $('.skinny').addClass('hidden');
+			$('.replacementSkinny, .titleRight').addClass('notHidden');
+        } else {
+            // Otherwise remove the navbar-ash class
+            $('.skinny').removeClass('hidden');
+			$('.replacementSkinny, .titleRight').removeClass('notHidden');
+        }
+    });
+}
+
 $(document).ready(function() {
 	
 	clipName();
 });
 
-function clipName() {
-	
-	var appDistance = $('#friend').offset().top;
-	
-    $("#parallax").scroll(function() {
-        // scrollTop gets the current vertical position of the scroll bar for the first element in the set of matched elements or set the vertical position of the scroll bar for every matched element.
-        if ($("#parallax").scrollTop() >= (appDistance - 40)) {
-            // If the window scrollbar position is greater or equal to the position of the 'app' element then add class navbar-ash to the navbar
-            $('.skinny').addClass('hidden');
-			$('.replacementSkinny').addClass('notHidden');
-			$('.titleRight').addClass('notHidden');
-        } else {
-            // Otherwise remove the navbar-ash class
-            $('.skinny').removeClass('hidden');
-			$('.replacementSkinny').removeClass('notHidden');
-			$('.titleRight').removeClass('notHidden');
-        }
-    });
-}
